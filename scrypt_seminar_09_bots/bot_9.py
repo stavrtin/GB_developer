4245e307500782f83d151f52007502b26741be19
Maksim Turchin <82442469+stavrtin@users.noreply.github.com>
2022-10-11 00:33:35 +0300

    Add files via upload

--- scrypt_seminar_09_bots/bot_9.py
+++ scrypt_seminar_09_bots/bot_9.py
@@ -2,7 +2,7 @@
 # pip install python-telegram-bot==v13.14
 
 from telegram import Update, Bot
-from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from random import randint
 from tok import token
 
@@ -10,6 +10,7 @@ bot_token = token
 bot = Bot(bot_token)
 updater = Updater(bot_token,  use_context=True)
 dispatcher = updater.dispatcher
+
 
 def start(update, content):
     content.bot.send_message(update.effective_chat.id, "Салют!")
@@ -22,14 +23,19 @@ def info(update, content):
 def roll(update, content):
     content.bot.send_message(update.effective_chat.id, text=str(randint(0,10)))
 
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
 start_heandler = CommandHandler('start', start)
 info_heandler = CommandHandler('info', info)
 roll_heandler = CommandHandler('roll', roll)
+text_roll_heandler = MessageHandler(Filters.text, echo)
 
 
 dispatcher.add_handler(start_heandler)
 dispatcher.add_handler(info_heandler)
 dispatcher.add_handler(roll_heandler)
+dispatcher.add_handler(text_roll_heandler)
 
 # ----- запускает цикл приема и обработки сообщений ----
 updater.start_polling()
---
+++ scrypt_seminar_09_bots/bot_9_3.py
@@ -0,0 +1,84 @@
+import logging
+from tok import token as TOKEN
+
+from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+# TOKEN = 'BOT_TOKEN'
+
+
+def start(update, context):
+    update.message.reply_text(
+        "Привет. Пройдите небольшой опрос, пожалуйста!\n"
+        "Вы можете прервать опрос, послав команду /stop.\n"
+        "В каком городе вы живёте?")
+
+    # Число-ключ в словаре states —
+    # втором параметре ConversationHandler'а.
+    return 1
+    # Оно указывает, что дальше на сообщения от этого пользователя
+    # должен отвечать обработчик states[1].
+    # До этого момента обработчиков текстовых сообщений
+    # для этого пользователя не существовало,
+    # поэтому текстовые сообщения игнорировались.
+
+
+def first_response(update, context):
+    # Это ответ на первый вопрос.
+    # Мы можем использовать его во втором вопросе.
+    locality = update.message.text
+    update.message.reply_text(
+        f"Какая погода в городе {locality}?")
+    # Следующее текстовое сообщение будет обработано
+    # обработчиком states[2]
+    return 2
+
+
+def second_response(update, context):
+    # Ответ на второй вопрос.
+    # Мы можем его сохранить в базе данных или переслать куда-либо.
+    weather = update.message.text
+    logger.info(weather)
+    update.message.reply_text("Спасибо за участие в опросе! Всего доброго!")
+    return ConversationHandler.END  # Константа, означающая конец диалога.
+    # Все обработчики из states и fallbacks становятся неактивными.
+
+
+def stop(update, context):
+    update.message.reply_text("Всего доброго!")
+    return ConversationHandler.END
+
+
+def main():
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    conv_handler = ConversationHandler(
+        # Точка входа в диалог.
+        # В данном случае — команда /start. Она задаёт первый вопрос.
+        entry_points=[CommandHandler('start', start)],
+
+        # Состояние внутри диалога.
+        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
+        states={
+            # Функция читает ответ на первый вопрос и задаёт второй.
+            1: [MessageHandler(Filters.text & ~Filters.command, first_response)],
+            # Функция читает ответ на второй вопрос и завершает диалог.
+            2: [MessageHandler(Filters.text & ~Filters.command, second_response)]
+        },
+
+        # Точка прерывания диалога. В данном случае — команда /stop.
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+
+    dp.add_handler(conv_handler)
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
