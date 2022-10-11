b63d6de9cd863e6289c016599446eef85adad099
Maksim Turchin <82442469+stavrtin@users.noreply.github.com>
2022-10-11 23:51:39 +0300

    Delete scrypt_lesson_bot directory

--- scrypt_lesson_bot/main.py
+++
@@ -1,8 +0,0 @@
-import bots
-
-
-def hello():
-    print('sadasd sdfsdf')
-
-if __name__ == '__main__':
-    bots
--- scrypt_lesson_bot/spy_data.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import datetime
-# import bots_commands
-
-def log_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    file_log = open('db.csv', 'a')
-    file_log.write(f'{datetime.datetime.now().time()}, {update.effective_user.first_name}, {update.effective_user.id}, {update.effective_user.last_name}, {update.chat_member}\n')
-    file_log.close()
