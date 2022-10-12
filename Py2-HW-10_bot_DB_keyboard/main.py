dfdb6e6595494abb329753ea5af1604a34934a8d
Maksim Turchin <82442469+stavrtin@users.noreply.github.com>
2022-10-12 22:35:57 +0300

    Delete Py2-HW-10_bot_DB_keyboard directory

--- Py2-HW-10_bot_DB_keyboard/main.py
+++
@@ -1,6 +0,0 @@
-import bot_9_db
-
-if __name__ == '__main__':
-    bot_9_db
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- Py2-HW-10_bot_DB_keyboard/tok.py
+++
@@ -1,2 +0,0 @@
-token = '5457794399:AAHZVbSVwyH8fmtjzkWojUyt5786Nipb6h8'
-token_db = '5742032741:AAHLh6oj4SDlFQwQRoxmQcvfBSWD8ywYidc'
